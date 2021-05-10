25cc78bc8ef813d99c6f480fd67a90e6c82e68f6
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-10 16:17:24 -0300

    Start backend project - basic configurations

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Application } from "express";
+import routes from "./routes";
+import mongoose from "mongoose";
+import cors from "cors";
+
+mongoose
+  .connect("mongodb://localhost:27017/test", { useNewUrlParser: true })
+  .then(() => {
+    const app: Application = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(routes);
+
+    app.listen(3333, () => console.log("O Servidor está rodando"));
+  });
