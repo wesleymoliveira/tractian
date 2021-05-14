2391db6891487f01d9808ea04986dd6ba12051c0
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-14 16:11:42 -0300

    Add assets into units schema

--- backend/src/models/Units.ts
+++ backend/src/models/Units.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface UnitsInterface extends Document {
   name: String;
   company: String;
+  assets?: Array<String>;
 }
 
 const UnitsSchema: Schema = new Schema({
   name: { type: String, required: true },
   company: { type: Schema.Types.ObjectId, ref: "Companies", required: true },
+  assets: [{ type: Schema.Types.ObjectId, ref: "Assets" }],
 });
 
 export default mongoose.model<UnitsInterface>("Units", UnitsSchema);
