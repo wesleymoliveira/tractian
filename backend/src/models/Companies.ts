ee204d417be989eba0f70a053f7ab375b89cba37
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-14 16:09:56 -0300

    Change schema set users and unities no required

--- backend/src/models/Companies.ts
+++ backend/src/models/Companies.ts
@@ -2,14 +2,14 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface CompaniesInterface extends Document {
   name: String;
-  users: Array<String>;
-  unities: Array<String>;
+  users?: Array<String>;
+  unities?: Array<String>;
 }
 
 const CompaniesSchema: Schema = new Schema({
   name: { type: String, required: true },
-  users: [{ type: Schema.Types.ObjectId, ref: "Users", required: true }],
-  unities: [{ type: Schema.Types.ObjectId, ref: "Units", required: true }],
+  users: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+  unities: [{ type: Schema.Types.ObjectId, ref: "Units" }],
 });
 
 export default mongoose.model<CompaniesInterface>("Companies", CompaniesSchema);
