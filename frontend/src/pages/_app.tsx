285c9db90fbfc6f3dfbb3b7bdb18ea9a040759f0
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-15 10:24:56 -0300

    Configure theme, fonts, colors

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,20 +1,23 @@
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
+import theme from 'styles/theme'
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title> Tractian - Frontend</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/favicon-ia.png" />
+        <link rel="apple-touch-icon" href="/img/favicon-ia.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="Tractian" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,58 @@
+export default {
+  grid: {
+    container: '93.5rem',
+  },
+  border: {
+    radius: '1.5rem',
+  },
+  font: {
+    family: 'Roboto',
+    light: 300,
+    normal: 'normal',
+    bold: 700,
+    semibold: 500,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.4rem',
+      xxlarge: '4.0rem',
+      huge: '6.0rem',
+    },
+  },
+  colors: {
+    primary: '#1890ff',
+    secondary: '#002766',
+    dark: '#06092B',
+    mainBg: '#FEFEFE',
+    white: '#FFFFFF',
+    black: '#707070',
+    orange: '#fa8c16',
+    yellow: '#ffec3d',
+    red: '#f5222d',
+    darkGray: '#A8A8B3',
+    gray: '#BFBFBF',
+    lightGray: '#EAEAEA',
+  },
+  spacings: {
+    xxsmall: '0.7rem',
+    xsmall: '1.6rem',
+    small: '2.0rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '5.0rem',
+    xxlarge: '10.0rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+  },
+} as const
