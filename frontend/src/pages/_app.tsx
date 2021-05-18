d216555e0dd76bb58d0abfd046c1eca85eab0b0e
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-18 15:34:34 -0300

    Create context with hook to deal with Add forms

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Provider as AuthProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import { ModalProvider } from '../context/ModalContext'
 import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
@@ -8,19 +9,21 @@ import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider session={pageProps.session}>
-        <Head>
-          <title> Tractian - Frontend</title>
-          <link rel="shortcut icon" href="/img/favicon-ia.png" />
-          <link rel="apple-touch-icon" href="/img/favicon-ia.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content="Tractian" />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ThemeProvider>
+    <ModalProvider>
+      <ThemeProvider theme={theme}>
+        <AuthProvider session={pageProps.session}>
+          <Head>
+            <title> Tractian - Frontend</title>
+            <link rel="shortcut icon" href="/img/favicon-ia.png" />
+            <link rel="apple-touch-icon" href="/img/favicon-ia.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="description" content="Tractian" />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ThemeProvider>
+    </ModalProvider>
   )
 }
 
