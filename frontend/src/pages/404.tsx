0d12cc13d268be5105c789e10cd1fb1345c8f288
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-21 00:37:50 -0300

    Create Page 404

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import Empty from 'components/Empty'
+
+export default function Page404() {
+  return (
+    <>
+      <Empty
+        title="404: Página não encontrada"
+        description="Ops... Desculpe, esta página não existe."
+        hasLink
+      />
+    </>
+  )
+}
