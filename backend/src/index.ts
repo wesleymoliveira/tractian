8d6d85ad5f6d722bc15ded3de32ac5de26ae7751
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-05-21 00:31:28 -0300

    Add Api documentation with swaggerApi

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Application } from "express";
 import routes from "./routes";
 import mongoose from "mongoose";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import * as swaggerDoc from "./docs/swagger.json";
 import uploadConfig from "./config/multerConfig";
 
 mongoose
@@ -17,6 +19,7 @@ mongoose
     app.use(express.json({ limit: "10mb" }));
     app.use(express.urlencoded({ extended: true, limit: "10mb" }));
     app.use("/images", express.static(uploadConfig.dest));
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
     app.use(routes);
 
     app.listen(3333, () => console.log("O Servidor está rodando"));
